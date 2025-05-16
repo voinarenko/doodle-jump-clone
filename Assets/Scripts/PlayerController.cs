0ac8e39b3cb3be0f17f62292138673427772cba8
Sergei Voinarenko <voynarenko@gmail.com>
2025-05-16 06:00:06 +0300

    Custom template added, movement changed to swipes.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-  public float moveSpeed = 10f;
   public float jumpForce = 15f;
   public float springJumpForce = 25f;
   public LayerMask platformLayer;
   public Transform feetCheck;
   public float feetRadius = 0.2f;
   public bool isGrounded;
+  public float swipeForce = 10f;
+  public float maxForce = 20f;
 
   private const float RelativeVelocityValue = 10;
   private const string PlatformTag = "Platform";
@@ -20,7 +21,8 @@ public class PlayerController : MonoBehaviour
   private SpriteRenderer _sr;
   private PlayerAnimator _playerAnimator;
   private float _horizontal;
-
+  private Vector2 _startTouch;
+  private bool _isSwiping;
 
   public void Start()
   {
@@ -75,23 +77,57 @@ public class PlayerController : MonoBehaviour
 
   private void Update()
   {
-#if UNITY_WEBGL && !UNITY_EDITOR
-    _horizontal = Input.acceleration.x * moveSpeed;
-#else
-    _horizontal = Input.GetAxis("Horizontal") * moveSpeed;
-#endif
+    if (Input.GetMouseButtonDown(0))
+    {
+      _startTouch = Input.mousePosition;
+      _isSwiping = true;
+    }
+    else if (Input.GetMouseButtonUp(0) && _isSwiping)
+    {
+      Vector2 endTouch = Input.mousePosition;
+      ApplySwipeForce(endTouch - _startTouch);
+      _isSwiping = false;
+    }
+    if (Input.touchCount > 0)
+    {
+      var touch = Input.GetTouch(0);
+      switch (touch.phase)
+      {
+        case TouchPhase.Began:
+          _startTouch = touch.position;
+          _isSwiping = true;
+          break;
+        case TouchPhase.Ended when _isSwiping:
+        {
+          var endTouch = touch.position;
+          ApplySwipeForce(endTouch - _startTouch);
+          _isSwiping = false;
+          break;
+        }
+      }
+    }
+  }
+
+  private void ApplySwipeForce(Vector2 swipeDelta)
+  {
+    if (Mathf.Abs(swipeDelta.x) < 20f) return;
+
+    var direction = Mathf.Sign(swipeDelta.x);
+    var strength = Mathf.Clamp(swipeDelta.magnitude / 100f, 0f, 1f);
+
+    var force = direction * strength * swipeForce;
+    force = Mathf.Clamp(force, -maxForce, maxForce);
+
+    _rb.linearVelocity = new Vector2(force, _rb.linearVelocity.y);
     FlipSprite();
   }
 
-  public void FixedUpdate()
-  {
+  public void FixedUpdate() =>
     isGrounded = Physics2D.OverlapCircle(feetCheck.position, feetRadius, platformLayer);
-    _rb.linearVelocity = new Vector2(_horizontal, _rb.linearVelocity.y);
-  }
 
   private void FlipSprite()
   {
-    _sr.flipX = _horizontal switch
+    _sr.flipX = _rb.linearVelocity.x switch
     {
       > 0 when _sr.flipX => false,
       < 0 when !_sr.flipX => true,
